39c8a46910181e6a4c9c9b4490943dc1277c213a
David <bischof.david.db@gmail.com>
2023-10-28 15:53:14 +0200

    added a loop that plays random sounds

--- src/Soundplayer.jsx
+++ src/Soundplayer.jsx
@@ -1,55 +1,54 @@
 import React, { useState, useEffect } from 'react';
-import { storage } from './firebase'; // Import the Firebase storage instance
 
 function AudioPlayer() {
   const [audioFiles, setAudioFiles] = useState([]);
   const [selectedAudio, setSelectedAudio] = useState(null);
 
   useEffect(() => {
-    // Fetch the list of audio files from Firebase Storage
-    const fetchAudioFiles = async () => {
-      const audioFilesList = [];
-      const storageRef = storage.ref('sounds'); // Update with your storage path
+    const context = require.context('./sounds', false, /\.mp3$/);
+    const audioFilesList = context.keys().map(context);
 
-      try {
-        const fileList = await storageRef.listAll();
-
-        for (const file of fileList.items) {
-          audioFilesList.push(await file.getDownloadURL());
-        }
-
-        setAudioFiles(audioFilesList);
-      } catch (error) {
-        console.error('Error fetching audio files:', error);
-      }
-    };
-
-    fetchAudioFiles();
+    setAudioFiles(audioFilesList);
   }, []);
 
   const playRandomAudio = () => {
-    // Select a random audio file
     const randomIndex = Math.floor(Math.random() * audioFiles.length);
     const randomAudio = audioFiles[randomIndex];
 
-    // Play the random audio
     const audioElement = new Audio(randomAudio);
     audioElement.play();
 
-    // Set the selected audio for reference
     setSelectedAudio(randomAudio);
   };
+
+  function RepeatSounds() {
+
+    for (let i = 0; i < 2; i++) {
+      setTimeout(() => {
+        console.log(i)
+      }, 5000);
+    }
+  }
+
+  function countToTen() {
+    let count = 1;
+    const interval = setInterval(function () {
+      playRandomAudio()
+      console.log(count)
+      count++;
+      if (count > 10) {
+        clearInterval(interval);
+      }
+    }, 10000);
+  }
+
+
 
   return (
     <div>
       <h1>Audio Player</h1>
       <button onClick={playRandomAudio}>Play Random Sound</button>
-
-      {selectedAudio && (
-        <audio controls>
-          <source src={selectedAudio} type="audio/mpeg" />
-        </audio>
-      )}
+      <button onClick={countToTen}>Play Random Sound infinity</button>
     </div>
   );
 }
