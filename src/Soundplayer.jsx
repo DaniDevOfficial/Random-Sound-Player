759aca8708cd0967002762ddb5fd769bfde6eb65
David <bischof.david.db@gmail.com>
2023-10-28 17:20:59 +0200

    added variables to the loop so the user can set on how many seconds between each sound

--- src/Soundplayer.jsx
+++ src/Soundplayer.jsx
@@ -3,7 +3,8 @@ import React, { useState, useEffect } from 'react';
 function AudioPlayer() {
   const [audioFiles, setAudioFiles] = useState([]);
   const [selectedAudio, setSelectedAudio] = useState(null);
-
+  const [secondBetween, setSecondBetween] = useState(null);
+  const [amountOfSounds, SetAmountOfSounds] = useState(null)
   useEffect(() => {
     const context = require.context('./sounds', false, /\.mp3$/);
     const audioFilesList = context.keys().map(context);
@@ -21,25 +22,18 @@ function AudioPlayer() {
     setSelectedAudio(randomAudio);
   };
 
+
   function RepeatSounds() {
-
-    for (let i = 0; i < 2; i++) {
-      setTimeout(() => {
-        console.log(i)
-      }, 5000);
-    }
-  }
-
-  function countToTen() {
     let count = 1;
     const interval = setInterval(function () {
       playRandomAudio()
+      console.log(amountOfSounds)
       console.log(count)
       count++;
-      if (count > 10) {
+      if (count > amountOfSounds) {
         clearInterval(interval);
       }
-    }, 10000);
+    }, secondBetween*1000);
   }
 
 
@@ -48,7 +42,10 @@ function AudioPlayer() {
     <div>
       <h1>Audio Player</h1>
       <button onClick={playRandomAudio}>Play Random Sound</button>
-      <button onClick={countToTen}>Play Random Sound infinity</button>
+      <button onClick={RepeatSounds}>Play Random Sound infinity</button>
+
+      <input type="number" placeholder="Seconds between each Sound" onChange={(e) => setSecondBetween(e.target.value)} value={secondBetween} />
+      <input type="number" placeholder="Amount of Sounds in total to be played" onChange={(e) => SetAmountOfSounds(e.target.value)} value={amountOfSounds} />
     </div>
   );
 }
